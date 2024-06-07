f663fa149c84c9b908cdc09463815ba62e0ce875
muradbuyukasik <10285764+muradbuyukasik@users.noreply.github.com>
2024-06-07 02:58:54 +0200

    created and moved systems into plugin

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,8 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
-        .add_systems(Startup, add_people)
-        .add_systems(Update, (hello_world, (update_people, greet_people).chain()))
+        .add_plugins((DefaultPlugins, HelloPlugin))
         .run();
-}
-
-fn hello_world() {
-    println!("hello world!");
 }
 
 #[derive(Component)]
@@ -18,23 +12,36 @@ struct Person;
 #[derive(Component)]
 struct Name(String);
 
-fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("Elaina Proctor".to_string())));
-    commands.spawn((Person, Name("Renzo Hume".to_string())));
-    commands.spawn((Person, Name("Zayna Nieves".to_string())));
-}
+pub struct HelloPlugin;
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("hello {}!", name.0);
-    }
-}
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, add_people)
+            .add_systems(Update, (hello_world, (update_people, greet_people).chain()));
 
-fn update_people(mut query: Query<&mut Name, With<Person>>) {
-    for mut name in &mut query {
-        if name.0 == "Elaina Proctor" {
-            name.0 = "Elaina Hume".to_string();
-            break; // We don’t need to change any other names
+        fn hello_world() {
+            println!("hello world!");
+        }
+
+        fn add_people(mut commands: Commands) {
+            commands.spawn((Person, Name("Elaina Proctor".to_string())));
+            commands.spawn((Person, Name("Renzo Hume".to_string())));
+            commands.spawn((Person, Name("Zayna Nieves".to_string())));
+        }
+
+        fn greet_people(query: Query<&Name, With<Person>>) {
+            for name in &query {
+                println!("hello {}!", name.0);
+            }
+        }
+
+        fn update_people(mut query: Query<&mut Name, With<Person>>) {
+            for mut name in &mut query {
+                if name.0 == "Elaina Proctor" {
+                    name.0 = "Elaina Hume".to_string();
+                    break; // We don’t need to change any other names
+                }
+            }
         }
     }
 }
